2917834f25b429c91ae667478ec5800905130d5e
k1o0b1a9 <keigo1019.yamakasi@gmail.com>
2023-07-09 10:46:40 +0900

    明細の取得方法を変更

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-require('dotenv').config()
+require('dotenv').config();
 const admin = require('firebase-admin');
+const fs = require('fs');
+const path = require('path');
+
 const serviceAccount = require('../path/card-specification-firebase-adminsdk-7zxaf-6c29653a2f.json');
 
 admin.initializeApp({
@@ -12,14 +15,22 @@ const csv_output = require('./csv_output.js');
 const extracting_element = require('./extracting_element.js');
 const aggregate_csv = require('./aggregate_csv.js');
 
+// specificationフォルダ内のファイルを取得
+const files = fs.readdirSync(process.env.SPECIFICATION);
+
+// ファイルを上から順に処理
 (async () => {
-    const dataFromCsv = await csv_output(process.env.CREDIT_SPEC_LOCATION);
+  for (const file of files) {
+    const filePath = path.join(process.env.SPECIFICATION, file);
 
-    const {newArray: detail_list, date} = await extracting_element(dataFromCsv);
+    const dataFromCsv = await csv_output(filePath);
 
-    const aggregate_result=await aggregate_csv(detail_list);
+    const { newArray: detail_list, date } = await extracting_element(dataFromCsv);
+
+    const aggregate_result = await aggregate_csv(detail_list);
 
     const docRef = db.collection('credit-card-statistics').doc(date);
 
     await docRef.set(aggregate_result);
+  }
 })();
